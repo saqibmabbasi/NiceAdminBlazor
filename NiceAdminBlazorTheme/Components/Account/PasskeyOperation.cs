be32cbfd2edf82a472a21a1fd1b301b55be02fa9
Saqib Mustafa Abbasi <saqibmabbasi@gmail.com>
2026-07-27 06:19:10 +0500

    HTML and CSS to Blazor
    - Project created
    - Theme included
    - Pages converted

---
+++ NiceAdminBlazorTheme/Components/Account/PasskeyOperation.cs
@@ -0,0 +1,8 @@
+namespace NiceAdminBlazorTheme.Components.Account
+{
+    public enum PasskeyOperation
+    {
+        Create = 0,
+        Request = 1,
+    }
+}
---
+++ NiceAdminBlazorTheme/Components/Layout/Entities/BreadcrumbItem.cs
@@ -0,0 +1,9 @@
+﻿namespace NiceAdminBlazorTheme.Components.Layout.Entities
+{
+    public class BreadcrumbItem
+    {
+        public string Text { get; set; }
+        public string Url { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
